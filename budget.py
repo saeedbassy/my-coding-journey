033a73490473819186f910b666327c7f03422a58
saeedbassy <saeedbassy5@gmail.com>
2026-07-04 10:31:55 -0400

    Add delete expense to budget.py

--- budget.py
+++ budget.py
@@ -9,12 +9,13 @@ try:
 except:
     pass
 
-while choice != "5":
+while choice != "6":
     print("1. Add an expense")
     print("2. View expenses")
     print("3. View total spent")
     print("4. View spending by category")
-    print("5. Quit")
+    print("5. Delete an expense")
+    print("6. Quit")
     choice = input("Choose a number: ")
     if choice == "1":
         expense_amount = float(input("Enter an expense amount: "))
@@ -42,3 +43,11 @@ while choice != "5":
             category_totals[master_category] = category_totals[master_category] + category_sum
         for master_category in category_totals:
             print("$" + str(category_totals[master_category]) + " " + master_category)
+    if choice == "5":
+        for index, user_expense in enumerate(expenses, start=1):
+            print(str(index) + ". " + "$" + str(user_expense["amount"]) + " " + user_expense["category"])
+        delete_expense = input("Enter a nunber to delete: ")
+        expenses.pop(int(delete_expense) - 1)
+        with open("expenses.txt", "w") as f:
+            for user_expense in expenses:
+                f.write(str(user_expense["amount"]) + "|" + user_expense["category"] + "\n")
