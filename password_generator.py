d5bbd94a917890aa1573d1ef68aba57c1d069f25
saeedbassy <saeedbassy5@gmail.com>
2026-07-06 19:51:11 -0400

    Add generate-another loop to password_generator.py

--- password_generator.py
+++ password_generator.py
@@ -1,10 +1,13 @@
 import random
-characters = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789!@#$%"
-random_password = []
-password_length = input("Enter your password's desired length in characters: ")
 def generate_password(characters):
-    for i in range(1, int(password_length) + 1):
-        random_password.append(random.choice(characters))
-    user_password = "".join(random_password)
-    return user_password
-print("Your password is: " + generate_password(characters))
+        for i in range(1, int(password_length) + 1):
+            random_password.append(random.choice(characters))
+        user_password = "".join(random_password)
+        return user_password
+keep_going = "yes"
+while keep_going == "yes":
+    characters = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789!@#$%"
+    random_password = []
+    password_length = input("Enter your password's desired length in characters: ")
+    print("Your password is: " + generate_password(characters))
+    keep_going = input("Generate other? (yes/no): ")
