9e11ded35add24704745029d64ac2e33de2c9235
saeedbassy <saeedbassy5@gmail.com>
2026-07-08 09:07:25 -0400

    Add day-of-week validation using a set in paycheck_calculator.py

--- paycheck_calculator.py
+++ paycheck_calculator.py
@@ -1,12 +1,16 @@
 shifts = []
 keep_going = "yes"
 while keep_going == "yes":
+    valid_days = {"Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"}
     work_day = input("Enter the day of the week: ")
-    work_hours = float(input("Enter the amount of hours: "))
-    work_rate = float(input("Enter the hourly rate: "))
-    user_shift = {"day": work_day, "hours": work_hours, "rate": work_rate}
-    shifts.append(user_shift)
-    keep_going = input("Do you want to add another shift? (yes/no): ")
+    if work_day.lower() in valid_days.lower():
+        work_hours = float(input("Enter the amount of hours: "))
+        work_rate = float(input("Enter the hourly rate: "))
+        user_shift = {"day": work_day, "hours": work_hours, "rate": work_rate}
+        shifts.append(user_shift)
+        keep_going = input("Do you want to add another shift? (yes/no): ")
+    else:
+        print("Invalid day of the week")
 total_earnings = 0
 for user_shift in shifts:
     total_earnings = float(total_earnings) + user_shift["hours"] * user_shift["rate"]
