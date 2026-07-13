bea67f75f174082975b9ca4aca2598b9878e2110
saeedbassy <saeedbassy5@gmail.com>
2026-07-13 16:43:08 -0400

    Add phone number validation to contact_book.py

--- contact_book.py
+++ contact_book.py
@@ -1,6 +1,7 @@
 contacts = []
 choice = "0"
 invalid_contact = ""
+invalid_phone = ""
 
 try:
     with open("contacts.txt", "r") as file:
@@ -19,16 +20,19 @@ while choice != "5":
     choice = input("Choose a number: ")
     if choice == "1":
         user_name = input("Enter a name: ")
-        if user_name != invalid_contact:
-            user_phone = input("Enter a phone number: ")
-            user_city = input("Enter a city: ")
-            user_contact = {"name": user_name, "info": {"phone": user_phone, "city": user_city}}
-            contacts.append(user_contact)
-            with open("contacts.txt", "w") as f:
-                for user_contact in contacts:
-                    f.write(user_contact["name"] + "|" + str(user_contact["info"]["phone"]) + "|" + user_contact["info"]["city"] + "\n")
-        else:
+        if user_name == invalid_contact:
             print("Invalid contact!")
+        else:
+            user_phone = input("Enter a phone number: ")
+            if user_phone == invalid_phone:
+                print("Invalid phone number!")
+            else:
+                user_city = input("Enter a city: ")
+                user_contact = {"name": user_name, "info": {"phone": user_phone, "city": user_city}}
+                contacts.append(user_contact)
+                with open("contacts.txt", "w") as f:
+                    for user_contact in contacts:
+                        f.write(user_contact["name"] + "|" + str(user_contact["info"]["phone"]) + "|" + user_contact["info"]["city"] + "\n")
     elif choice == "2":
         for user_contact in contacts:
             print(f"{user_contact["name"]}, {user_contact["info"]["phone"]}, {user_contact["info"]["city"]}")
